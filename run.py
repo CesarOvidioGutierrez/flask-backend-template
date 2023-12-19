4888557e86b812d078402d9604be0860c17625a1
Cesar Gutierrez <cesar.gutierrez@codeait.com>
2023-12-19 18:02:22 -0300

    feat: Add create_app with the Factory Pattern Implemented the create_app function to initialize the Flask application using the Factory Pattern.

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-from app import app
-from app.routes.auth import auth_blueprint
+from app import create_app
 
-app.register_blueprint(auth_blueprint, url_prefix='/api', name='auth')
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
